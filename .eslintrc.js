5d81f3201725fd5f22fbe44bc4a0033f599a80a3
Rabih Maalouf <amoun00@gmail.com>
2026-02-19 08:18:01 +0200

    whatever 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,17 @@ module.exports = {
     {
       files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: [
+          './tsconfig.base.json',
+          './apps/*/tsconfig.json',
+          './packages/*/tsconfig.json',
+        ],
+        tsconfigRootDir: __dirname,
+      },
       plugins: ['@typescript-eslint'],
       extends: [
         'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
       ],
       rules: {
         '@typescript-eslint/no-explicit-any': 'error',           // Rule 6: No any
